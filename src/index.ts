5c3d74b9bb7c6a8ca84887fb67d07e57f9f92f09
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-09-14 16:48:55 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import { AddressInfo } from 'net';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/', userRouter);
 app.use('/photo', photoRouter);
 
