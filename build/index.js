6645f1f339f26096210547a97703f21c1f99a08e
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-09-14 17:08:24 -0300

    adjust cors

--- build/index.js
+++ build/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const UserRouter_1 = require("./Router/UserRouter");
@@ -10,6 +11,7 @@ const PhotoRouter_1 = require("./Router/PhotoRouter");
 dotenv_1.default.config();
 const app = express_1.default();
 app.use(express_1.default.json());
+app.use(cors_1.default());
 app.use('/', UserRouter_1.userRouter);
 app.use('/photo', PhotoRouter_1.photoRouter);
 const server = app.listen(3000, () => {
